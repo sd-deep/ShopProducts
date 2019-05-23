3fcf0bd14aa0e5d59857d1398d7a7a058329004b
Shankhadeep Das <shankhadeepdas8@gmail.com>
2019-05-23 13:03:07 +0200

    model for cart with sequelize

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const bodyParser = require('body-parser');
 const sequelize = require('./util/database')
 const Product = require('./models/product');
 const User = require('./models/user')
+const Cart = require('./models/cart')
+const CartItem = require('./models/cart-item')
 
 const app = express();
 
@@ -47,9 +49,13 @@ app.use(errorController.getErrorPage);
 
 Product.belongsTo(User, {constraints : true, onDelete :'CASCADE'})
 User.hasMany(Product)
+User.hasOne(Cart)
+//Cart.belongsTo(User) // this is not required as both this and User.hasOne(Cart) are same
+Cart.belongsToMany(Product,{ through : CartItem})
+Product.belongsToMany(Cart,{ through : CartItem})
 
 
-sequelize.sync()
+sequelize.sync( {force : true}) // {force : true } for new associaltion rules
     .then((result)=>{
         User.findByPk(1)
         //console.log(result)
