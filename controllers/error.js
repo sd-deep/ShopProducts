e9f8cad3df297e1600fb89b108a515cb93ed556e
sd-deep <shankhadeepdas8@gmail.com>
2019-06-01 12:04:24 +0530

    deleting session

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,4 @@
 exports.getErrorPage = (req, res, next) => {
     res.status(404).render('404', {pageTitle : 'Page not found',path:'/404',isAuthenciated : req.session});
 }
+//there has been changes here
